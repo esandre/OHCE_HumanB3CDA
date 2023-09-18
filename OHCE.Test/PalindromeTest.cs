33b19e9d63723042d370de6de5a2b84f8c51d1b5
Enzo Sandré <atelier@enzosandre.fr>
2023-09-18 13:45:08 +0200

    Verbose sur le test

--- OHCE.Test/PalindromeTest.cs
+++ OHCE.Test/PalindromeTest.cs
@@ -1,9 +1,17 @@
 using OHCE.Domaine;
+using Xunit.Abstractions;
 
 namespace OHCE.Test
 {
     public class PalindromeTest
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public PalindromeTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Theory]
         [InlineData("")]
         [InlineData("test")]
@@ -18,6 +26,7 @@ namespace OHCE.Test
             // ALORS on obtient son miroir
             var miroir = new string(chaîne.Reverse().ToArray());
             Assert.Equal(miroir, obtenu);
+            _testOutputHelper.WriteLine(obtenu);
         }
     }
 }
